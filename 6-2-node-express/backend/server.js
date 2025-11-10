7c0e47b87b52222e6f7d80928a03c29c850bbbb5
Your GitHub Name <lujain.aljehani@outlook.com>
2025-11-10 11:07:35 +0300

    TODO1

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -2,10 +2,13 @@ import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
-{/*write code for cors*/}
+const app = express();
+app.use(cors());
 
 
 {/*write code to define routes*/}
 
 
-{/*write code to create server*/}
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
