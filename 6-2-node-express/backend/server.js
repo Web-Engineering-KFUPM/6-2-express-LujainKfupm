1b960ec746b99e7ff3fd8e56678fc40d3a9dc7a2
Your GitHub Name <lujain.aljehani@outlook.com>
2025-11-10 11:26:44 +0300

    TODO4&5

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -3,10 +3,17 @@ import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
 const app = express();
+const PORT = 3000;
 app.use(cors());
 
 
-{/*write code to define routes*/}
+app.get("/", (req, res) => {
+    res.send("Welcome to the Quote Generator API");
+});
+app.get("/api/quote", (req, res) => {
+    const quote = getRandomQuote();
+    res.json({ quote });
+});
 
 
 app.listen(PORT, () => {
